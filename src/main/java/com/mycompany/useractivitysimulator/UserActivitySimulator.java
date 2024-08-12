007761ce3262fdee572094afce1aca7bd06dbcc6
JCHDeveloperGuy <jchelton1@gmail.com>
2024-08-12 11:31:25 -0500

    updates

--- src/main/java/com/mycompany/useractivitysimulator/UserActivitySimulator.java
+++ src/main/java/com/mycompany/useractivitysimulator/UserActivitySimulator.java
@@ -1,69 +1,65 @@
 package com.mycompany.useractivitysimulator;
 
-import org.apache.kafka.clients.producer.KafkaProducer; 
-import org.apache.kafka.clients.producer.ProducerConfig; 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord; 
 import org.apache.kafka.common.serialization.StringSerializer;  
 import java.util.*; 
  
 public class UserActivitySimulator {
-    
-    public String UserId;
 
-    static List<String> lstUsers = Arrays.asList(
-            "User1", 
-            "User2",
-            "User3",
-            "User4",
-            "User5",
-            "User6"
-    );
-    
     public static void main(String[] args) {
+
+        final List<String> lstUsers = Arrays.asList(
+                "User1",
+                "User2",
+                "User3",
+                "User4",
+                "User5",
+                "User6"
+        );
         
-        //create producer on a new thread each time we create a new user
-        
-        for (int i = 0; i <= lstUsers.size(); i++) {
-            User.CreateUser();
+        for (int i = 0; i <= lstUsers.size() - 1; i++) {
+            myUser newUser = new myUser();
+            newUser.UserName = lstUsers.get(i);
+            newUser.start();
         }
     }
 }
 
-class User {
-    
-    static void CreateUser() {
-        
-        SpawnProducer();
-        
-        CreateNewUser();
-        
-        
-        
-    }
-    
-    
-   static void CreateNewUser(){
-       
-       
-   } 
-    
-   static KafkaProducer<String, String> SpawnProducer() {
-        Properties properties = new Properties(); 
-        
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); 
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); 
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); 
-        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy"); 
-        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20"); 
-        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024)); 
-  
-        KafkaProducer<String, String> producer = new KafkaProducer<>(properties); 
-  
-        ProducerRecord<String, String> record = new ProducerRecord<>("UserActivitySimulator", "hello from the producer"); 
-           
-        producer.send(record);
-        producer.flush(); 
-        
-        return producer;
+class myUser extends Thread {
+    String UserName;
+
+    @Override
+    public void run() {
+        System.out.println(STR."User is \{UserName}");
     }
 }
+
+class  myProducer  {
+
+    public static void main(String[] args) {
+        CreateProducer();
+    }
+
+    private static void  CreateProducer() {
+        Properties props = new Properties();
+
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        props.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        props.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024));
+
+        Producer<String, String> producer = new KafkaProducer
+                <String, String>(props);
+
+
+        producer.send(new ProducerRecord<String, String>("UserActivitySimulator", "hello from the producer"));
+        //System.out.println('“'Message sent successfully”)
+        producer.close();
+    }
+}
+
